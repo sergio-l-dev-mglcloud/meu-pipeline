580d8828ca995d114ea92d7a18b447e0ca37f5cc
Sergio <sergiolimacontabilidade@gmail.com>
2026-07-17 08:30:19 -0300

    Adiciona aplicacao Flask e Dockerfile

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "application": "Meu Pipeline",
+        "version": "1.0.0",
+        "provider": "Magalu Cloud"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "UP"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
